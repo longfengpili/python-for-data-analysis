dcf9ce2ee3700c7dd881d21b3110ca3cb94ad6cd
longfengpili <398745129@qq.com>
2018-06-05 08:12:51 +0800

    t

---
+++ pydata-book_材料/examples/ipython_log.py
@@ -0,0 +1,23 @@
+# IPython log file
+
+get_ipython().run_line_magic('loagstart', '')
+get_ipython().run_line_magic('logstart', '')
+import pandas-reader import data,web
+import pandas_reader import data,wb
+from pandas_reader import data,wb
+from pandas.io import data,wb
+from pandas_datareader import data,wb
+for ticker in ['AAPL','IBM','MSFT','GOOG']:
+    all_data[ticker]=data.get_data_yahoo(ticker,'1/1/2010','1/1/2012')
+for ticker in ['AAPL','IBM','MSFT','GOOG']:
+    all_data[ticker]=wb.get_data_yahoo(ticker,'1/1/2010','1/1/2012')
+    
+for ticker in ['AAPL','IBM','MSFT','GOOG']:
+    all_data[ticker]=data.get_data_yahoo(ticker,'1/1/2010','1/1/2012')
+    
+    
+for ticker in ['AAPL','IBM','MSFT','GOOG']:
+    all_data[ticker]=data.get_data_yahoo(ticker,'1/1/2010','1/1/2012')
+    
+    
+data.get_data_yahoo_actions()
